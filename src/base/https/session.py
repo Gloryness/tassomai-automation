e0a36fdd922f6b692b8113ce4e25538183384a65
Gloryness <gravity50ml@gmail.com>
2021-02-26 07:48:55 +0000

    v2021.02.26 - fixed the database getting too big

--- src/base/https/session.py
+++ src/base/https/session.py
@@ -150,8 +150,8 @@ class Session(QObject):
                         if self.until == self.until_incorrect:
                             self.until = 0
                             force_incorrect = True
-                    if '  ' in question:
-                        question = question.replace("  ", " ")
+                    if '  ' in question['text']:
+                        question['text'] = question['text'].replace("  ", " ")
                     question_data, database = await self.tassomai.answer_question(Variables(question, force_incorrect))
                     item = self.ui.ui.table.item(self.row, 0)
                     if item is None:
